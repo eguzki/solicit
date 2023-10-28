0e8cd0cb0d709b8bd5f5fd94788d2f3bb70aa338
Eguzki Astiz Lezaun <eastizle@redhat.com>
2023-10-28 12:37:18 +0200

    [wip] support tls

--- examples/client_simple.rs
+++ examples/client_simple.rs
@@ -4,76 +4,79 @@
 //! that performs all IO in the main thread.
 
 extern crate solicit;
+extern crate url;
 
 use std::env;
 use std::str;
+use url::Url;
 
-use solicit::http::{HttpResult, Response, StreamId};
-use solicit::http::client::CleartextConnector;
 use solicit::client::SimpleClient;
+use solicit::http::client::tls::TlsConnector;
+use solicit::http::client::CleartextConnector;
+use solicit::http::{HttpResult, Response};
 
-fn fetch<'a>(host: &'a str, port: u16, paths: &'a [&'a str]) -> (HttpResult<Response<'a, 'a>>, Vec<HttpResult<()>>) {
-    let mut client = SimpleClient::with_connector(CleartextConnector::with_port(host, port)).unwrap();
+fn fetch<'a>(
+    scheme: &'a str,
+    host: &'a str,
+    port: u16,
+    paths: &'a [&'a str],
+) -> (HttpResult<Response<'a, 'a>>, Vec<HttpResult<()>>) {
+    let connector = match scheme {
+        "https" => TlsConnector::with_port(host, port),
+        _ => CleartextConnector::with_port(host, port),
+    };
 
-    let results = paths.iter().map(|path| {
-        let stream_id = client.head(b"GET", path.as_bytes())?;
-        client.reset(stream_id)
-    }).collect();
+    let mut client = SimpleClient::with_connector(connector).unwrap();
+
+    let results = paths
+        .iter()
+        .map(|path| {
+            let stream_id = client.head(b"GET", path.as_bytes())?;
+            client.reset(stream_id)
+        })
+        .collect();
     let err = client.get_response(((paths.len() * 2) - 1) as u32);
     (err, results)
 }
 
 fn main() {
     fn print_usage() {
-        println!("Usage: client_simple <host>[:<port>] <path> <hits>");
-        println!(
-            "NOTE: The example does not accept URLs, rather the host name and a list of paths");
+        println!("Usage: client_simple <url> <path> <hits>");
     }
 
-    let host = env::args().nth(1);
-    let path = env::args().nth(2);
-    let size = env::args().nth(3).map_or(100, |s| s.parse::<i32>().unwrap());
+    let args: Vec<String> = env::args().collect();
 
-    if host.is_none() || path.is_none() {
+    if args.len() != 4 {
+        println!("Invalid args!");
         print_usage();
         return;
     }
 
-    let path = path.unwrap();
+    let url = &args[1];
+    let path = &args[2];
+    let size = args[3].parse::<i32>().unwrap();
+
+    let url_obj = Url::parse(url).unwrap();
 
     let mut dups: Vec<&str> = Vec::with_capacity(size as usize);
     for _ in 0..size {
         dups.push(&path);
     }
 
-
-    let host = host.unwrap();
-    // Split off the port, if present
-    let parts: Vec<_> = host.split(":").collect();
-    if parts.len() > 2 {
-        println!("Invalid host!");
-        print_usage();
-        return;
-    }
-
-    let (host, port) = if parts.len() == 1 {
-        (parts[0], 80)
-    } else {
-        let port = match str::FromStr::from_str(parts[1]) {
-            Err(_) => {
-                println!("Invalid host (invalid port given)");
-                print_usage();
-                return;
-            }
-            Ok(port) => port,
-        };
-        (parts[0], port)
-    };
+    let host = url_obj.host_str().unwrap();
+    let scheme = url_obj.scheme();
+    let port = url_obj.port().unwrap_or(match scheme {
+        "https" => 443,
+        _ => 80,
+    });
 
     println!("Trying {} fast HEAD/RST frames to {}", size, host);
 
-    let (response, results) = fetch(&host, port, &dups);
-    println!("Sent {} HEAD/RST alright...", results.iter().filter(|r| r.is_ok()).count());
+    let (response, results) = fetch(scheme, &host, port, &dups);
+    println!(
+        "Sent {} HEAD/RST alright...",
+        results.iter().filter(|r| r.is_ok()).count()
+    );
     match response {
         Ok(_) => println!("We got to the end ok apparently, that might be bad..."),
         Err(err) => println!("Couldn't get to reset all streams ok: {:?}", err),
